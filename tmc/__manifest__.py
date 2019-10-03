569f5f6fcfb38256477a6245ec8895b26ead2162
Liso Gallo <liso@riseup.net>
2019-10-03 14:39:21 -0300

    [MIG] Module version

--- tmc/__manifest__.py
+++ tmc/__manifest__.py
@@ -2,7 +2,7 @@
 {
     'name': "TMC Base",
     'summary': 'Main TMC models and functionality',
-    'version': '10.0.1.0.0',
+    'version': '13.0.1.0.0',
     'website': 'https://www.tmcrosario.gob.ar',
     'author': 'Tribunal Municipal de Cuentas - Municipalidad de Rosario',
     'license': 'AGPL-3',
