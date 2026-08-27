ad8daaba2727ff433a17f1f1397934579f08452a
Lisandro Gallo <4266083+lisandrogallo@users.noreply.github.com>
2026-08-27 08:58:48 -0300

    [IMP] tmc.system: add color and enforce a unique name
    - Add a color field so systems can be told apart visually in views
    - Order systems by name and make the name required
    - Add a UNIQUE(name) constraint with a translated message
    - Update the es_AR translation for the new field and constraint

--- tmc/models/system.py
+++ tmc/models/system.py
@@ -4,5 +4,9 @@ from odoo import fields, models
 class System(models.Model):
     _name = "tmc.system"
     _description = "System"
+    _order = "name"
 
-    name = fields.Char()
+    name = fields.Char(required=True)
+    color = fields.Integer()
+
+    _name_unique = models.Constraint("UNIQUE(name)", "System name must be unique")
